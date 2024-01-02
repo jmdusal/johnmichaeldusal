c7ee5494f33305d924d9190769b1d2826028cc50
jmdusal <jmmichaeldusal@gmail.com>
2024-01-02 12:19:04 +0800

    first commit

---
+++ src/data/user.js
@@ -0,0 +1,131 @@
+const INFO = {
+	main: {
+		title: "John Michael R. Dusal",
+		name: "John Michael",
+		email: "jmmichaeldusal@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "John Michael R. Dusal",
+		description:
+			"I am a web developer with expertise in Laravel framework. I have experience in building scalable, secure and reliable web applications using laravel framework. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+
+			// Innovative, task-driven with 2 years and 7 months of experience in web development, Proficient in developing web applications, testing codes, simple/complex issues, and implementing new features based on user feedback.
+			// Innovative, task-driven with 2 years and 7 months of experience in web development, Proficient in developing web applications, I enjoy solving simple/complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.
+
+
+
+			// I am a web developer with expertise in Laravel framework. I have experience in building scalable, secure and reliable web applications using laravel framework. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.
+
+	},
+
+	about: {
+		title: "I’m John Michael R. Dusal ",
+		description:
+			"I've worked on a variety of projects over 2 years and 7 months and I'm proud of the progress I've made. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+
+			// Innovative, task-driven with 2 years and 7 months of experience in web development, Proficient in developing web applications, testing codes, simple/complex issues, and implementing new features based on user feedback.
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Kauswagan Media",
+			description:
+				"A content management system that includes news, stories, images, and videos from all barangays in Kauswagan Municipality.",
+			// logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://bitbucket.org/jmdusal/kauswagan-web/src/master/",
+		},
+
+		{
+			title: "StrymGlobal Website",
+			description:
+				"A website can send emails with file upload and can also send messages to the employer of strymglobal.",
+			// logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://bitbucket.org/jmdusal/strymglobal/src/master/",
+		},
+
+		{
+			title: "E-Barangay Content Management System",
+			description:
+				"Includes news, stories, images, and videos from all barangays in Municipality.",
+				// This is a standalone system built in 2 different php frameworks CodeIgniter and Laravel with 2 developers developed
+			// logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://bitbucket.org/jmdusal/bislig/src/master/",
+		},
+
+		{
+			title: "HR App",
+			description:
+				"Contains Employee Management System, Evaluation and Ranking System, Attendance System, and Payroll System.",
+			// logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://biznizaide.com/",
+		},
+
+		{
+			title: "Kure",
+			description:
+				"A Project Management System automates project creation and documentation.",
+			// logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://kureko.com/",
+		},
+
+		{
+			title: "EliteInsure Registers",
+			description:
+				"An Information Management System contains all data in eliteinsure.",
+			// logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "http://registers-beta.onlineinsure.co.nz/",
+		},
+
+		{
+			title: "CARRD",
+			description:
+				"An Information Management System.",
+			// logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/lilypaddigital/carrd",
+		},
+
+		{
+			title: "SERPP",
+			description:
+				"An Information Management System.",
+			// logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/lilypaddigital/serpp-php8",
+		},
+
+		{
+			title: "PIDS",
+			description:
+				"An Information Management System.",
+			// logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/lilypaddigital/pids-php8",
+		},
+	],
+};
+
+export default INFO;
